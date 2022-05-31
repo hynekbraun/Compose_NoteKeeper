15650f10f9a87500a6cfdb5fc67e74aec7db9a09
resonus <braun.hynek@gmail.com>
2022-05-31 23:46:08 +0200

    Added font family, but had issues with implementing it, will come back to it later

--- app/src/main/java/com/hynekbraun/composenotekeeper/ui/theme/Type.kt
+++ app/src/main/java/com/hynekbraun/composenotekeeper/ui/theme/Type.kt
@@ -2,9 +2,13 @@ package com.hynekbraun.composenotekeeper.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hynekbraun.composenotekeeper.R
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -13,16 +17,14 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+)
+
+val ubuntuFontFamily = FontFamily(
+    fonts = listOf(
+    Font(R.font.ubuntu_regular, weight = FontWeight.Normal),
+    Font(R.font.ubuntu_bold, weight = FontWeight.Bold),
+    Font(R.font.ubuntu_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.ubuntu_light, weight = FontWeight.Light),
+    Font(R.font.ubuntu_medium, weight = FontWeight.Medium)
     )
-    */
 )
